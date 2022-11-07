f7f3c1ff3d753d99ee1d728904a2b0cb5334653f
Ewerton F S Marinho <53940331+ewertonfelipee@users.noreply.github.com>
2022-11-07 10:02:25 -0300

    First Commit

---
+++ Spring-Course/src/main/java/ewertonfelipee/github/io/VendasApplication.java
@@ -0,0 +1,21 @@
+package ewertonfelipee.github.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.*;
+
+@SpringBootApplication
+@RestController
+public class VendasApplication {
+
+    @GetMapping("/hello")
+    public String HelloWorld(){
+        return "hello world";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class, args);
+    }
+}
